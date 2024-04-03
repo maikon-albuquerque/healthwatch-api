1d3d21b38035f1597e454d620f434095b8e20415
Maikon Albuquerque <maikonmorais@gmail.com>
2024-04-03 17:51:38 -0300

    feat: add audit annotation

--- src/main/java/com/github/maikonalbuquerque/healthwatchapi/HealthwatchApiApplication.java
+++ src/main/java/com/github/maikonalbuquerque/healthwatchapi/HealthwatchApiApplication.java
@@ -2,8 +2,10 @@ package com.github.maikonalbuquerque.healthwatchapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HealthwatchApiApplication {
 
     public static void main(String[] args) {
