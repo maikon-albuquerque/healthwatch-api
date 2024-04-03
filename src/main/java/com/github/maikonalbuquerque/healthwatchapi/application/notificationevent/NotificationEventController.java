1f084c874ae497ccb0e71c3602d9c923d7df3275
Maikon Albuquerque <maikonmorais@gmail.com>
2024-04-03 17:52:29 -0300

    feat: create controller NotificationEventController

---
+++ src/main/java/com/github/maikonalbuquerque/healthwatchapi/application/notificationevent/NotificationEventController.java
@@ -0,0 +1,9 @@
+package com.github.maikonalbuquerque.healthwatchapi.application.notificationevent;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/events")
+public class NotificationEventController {
+}
