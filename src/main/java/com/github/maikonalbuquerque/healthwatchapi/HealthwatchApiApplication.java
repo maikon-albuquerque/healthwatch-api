f6786b1aa977dc1f2d1a968b30e7bf7914287c78
Maikon Albuquerque <maikonmorais@gmail.com>
2024-04-02 22:49:43 -0300

    raw: initial project setup

---
+++ src/main/java/com/github/maikonalbuquerque/healthwatchapi/HealthwatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.maikonalbuquerque.healthwatchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthwatchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthwatchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/maikonalbuquerque/healthwatchapi/HealthwatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maikonalbuquerque.healthwatchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthwatchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
