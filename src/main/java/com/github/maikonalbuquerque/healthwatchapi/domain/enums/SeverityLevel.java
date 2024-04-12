f2d663a66fa6519e8bef2e749aaed0be14435dae
Maikon Albuquerque <maikonmorais@gmail.com>
2024-04-12 18:10:21 -0300

    feat: create SeverityLevel and Shift enum

---
+++ src/main/java/com/github/maikonalbuquerque/healthwatchapi/domain/enums/SeverityLevel.java
@@ -0,0 +1,11 @@
+package com.github.maikonalbuquerque.healthwatchapi.domain.enums;
+
+import javax.swing.*;
+
+public enum SeverityLevel {
+    UM,
+    DOIS,
+    TRES,
+    QUATRO,
+    CINCO
+}
---
+++ src/main/java/com/github/maikonalbuquerque/healthwatchapi/domain/enums/Shift.java
@@ -0,0 +1,8 @@
+package com.github.maikonalbuquerque.healthwatchapi.domain.enums;
+
+public enum Shift {
+
+    MANHÃ,
+    TARDE,
+    NOITE
+}
