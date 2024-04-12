04b033058c14b8967c7304e679471c05f85e4476
Maikon Albuquerque <maikonmorais@gmail.com>
2024-04-12 18:09:31 -0300

    feat: create CauseCategory entity

---
+++ src/main/java/com/github/maikonalbuquerque/healthwatchapi/domain/entities/CauseCategory.java
@@ -0,0 +1,27 @@
+package com.github.maikonalbuquerque.healthwatchapi.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class CauseCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+
+}
